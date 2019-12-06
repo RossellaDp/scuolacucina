d7d92a80c12ccf89a88af86ecc72203ca8c33dc7
Administrator <Administrator@AECI-DZKD2X2>
2019-12-06 12:57:23 +0100

    Alice: metodiDao Catalogo

--- src/dao/CatalogoDAOImpl.java
+++ src/dao/CatalogoDAOImpl.java
@@ -1,12 +1,16 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 import entity.Categoria;
 import entity.Corso;
 import entity.Feedback;
+import entity.Utente;
 import exceptions.ConnessioneException;
 
 public class CatalogoDAOImpl implements CatalogoDAO {
@@ -22,8 +26,15 @@ public class CatalogoDAOImpl implements CatalogoDAO {
 	 */
 	@Override
 	public void insert(Corso corso) throws SQLException {
-		// TODO Auto-generated method stub
-
+		PreparedStatement ps=conn.prepareStatement("INSERT INTO catalogo(id_corso,titolo,id_categoria,numeroMaxPartecipanti,costo,descrizione) VALUES (?,?,?,?,?,?)");
+		ps.setInt(1, corso.getCodice());
+		ps.setString(2, corso.getTitolo());
+		ps.setInt(2, corso.getCodice());
+		ps.setInt(4, corso.getMaxPartecipanti());
+		ps.setDouble(5, corso.getCosto());
+		ps.setString(6, corso.getDescrizione());
+	
+		ps.executeUpdate();
 	}
 
 	/*
@@ -33,9 +44,21 @@ public class CatalogoDAOImpl implements CatalogoDAO {
 	 */
 	@Override
 	public void update(Corso corso) throws SQLException {
-		// TODO Auto-generated method stub
+		PreparedStatement ps=conn.prepareStatement("UPDATE catalogo SET titolo=?, id_categoria=?, numeroMaxPartecipanti=?, costo=?, descrizione=? where id_corso=?");
+		ps.setString(1, corso.getTitolo());
+		ps.setInt(2, corso.getIdCategoria());
+		ps.setInt(3, corso.getMaxPartecipanti());
+		ps.setDouble(4, corso.getCosto());
+		ps.setString(5, corso.getDescrizione());
+		ps.setInt(6, corso.getCodice());
+		
+		int n = ps.executeUpdate();
+		if(n==0)
+			throw new SQLException("corso: " + corso.getCodice() + " non presente");
 
 	}
+
+	
 
 	/*
 	 * cancellazione di un nuovo corso nel catalogo dei corsi
@@ -45,7 +68,12 @@ public class CatalogoDAOImpl implements CatalogoDAO {
 	 */
 	@Override
 	public void delete(int idCorso) throws SQLException {
-		// TODO Auto-generated method stub
+		PreparedStatement ps = conn.prepareStatement("DELETE FROM catologo WHERE id_corso=?");
+		ps.setInt(1, idCorso);
+		int n = ps.executeUpdate();
+		if(n==0)
+			throw new SQLException("utente " + idCorso + " non presente");
+		    throw new SQLException ("corso" + idCorso + "non cancellabile");
 
 	}
 
@@ -55,8 +83,23 @@ public class CatalogoDAOImpl implements CatalogoDAO {
 	 */
 	@Override
 	public ArrayList<Corso> select() throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Corso> corsi = new ArrayList<Corso>(); 
+
+		PreparedStatement ps=conn.prepareStatement("SELECT * FROM catalogo");
+
+		ResultSet rs = ps.executeQuery();
+		while(rs.next()){
+			Corso corso = new Corso();
+			corso.setCodice(rs.getInt("id_corso"));
+			corso.setTitolo(rs.getString("titolo"));
+			corso.setIdCategoria(rs.getInt("id_categoria"));
+			corso.setMaxPartecipanti(rs.getInt("numeroMaxPartecipanti"));
+			corso.setCosto(rs.getInt("costo"));
+			corso.setDescrizione(rs.getString("descrizione"));
+			corsi.add(corso);
+		}
+
+		return corsi;
 	}
 
 	/*
@@ -65,9 +108,37 @@ public class CatalogoDAOImpl implements CatalogoDAO {
 	 */
 	@Override
 	public Corso select(int idCorso) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+
+		PreparedStatement ps=conn.prepareStatement("SELECT * FROM catalogo where id_corso =?");
+
+		ps.setInt(1, idCorso);
+
+		ResultSet rs = ps.executeQuery();
+		Corso corso =null;
+		if(rs.next()){
+			
+			corso.setTitolo(rs.getString("titolo"));
+			corso.setIdCategoria(rs.getInt("id_categoria"));
+			corso.setMaxPartecipanti(rs.getInt("numeroMaxPartecipanti"));
+			corso.setCosto(rs.getInt("costo"));
+			corso.setDescrizione(rs.getString("descrizione"));
+			corso.setCodice(rs.getInt("id_corso"));
+		
+			
+
+			
+			return corso;
+		}
+	
+		else {
+			throw new SQLException("corso: " + idCorso + " non presente");
+		
+		
 	}
 
 
+
 }
+}
+
+
